554fc78cbb4feb837701e69a1193708245aad3e2
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2023-01-31 18:30:26 +0000

    Components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import styles from "@/components/Header/index.module.css";
+
+export default function Header() {
+  return <div className={styles.container}>React Synonyms Project</div>;
+}
---
+++ src/pages/heading/index.tsx
@@ -0,0 +1,7 @@
+import styles from "@/pages/heading/index.module.css";
+
+const heading = () => {
+  return <div className={styles.container}>React Synonyms Project</div>;
+};
+
+export default heading;
